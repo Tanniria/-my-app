d0b8479282909a6753150ecc76615cd7d75e1f02
Татьяна К. Смакова <smakova@corp1.ogacho.ru>
2024-07-23 16:27:45 +0500

    new

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,14 +8,48 @@ export default function Section() {
         <div className="box">
           <div className="title">
             <span className="block"></span>
-            <h1>
-              Разделы
-            </h1>
+            <h1>Разделы</h1>
           </div>
 
           <div className="role">
             <div className="block"></div>
-            <p>Виртуальной выставки<span></span></p>
+            <p>
+              Виртуальной выставки<span></span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="main-container">
+        <div class="card-carousel">
+          <div class="card" id="1">
+            <div class="image-container"></div>
+            <p class="image-container__name">
+              <a href="#leader" class="link">
+                Становление Челябинской области
+              </a>
+            </p>
+          </div>
+          <div class="card" id="2">
+            <div class="image-container"></div>
+            <p class="image-container__name">
+              Руководители
+            </p>
+          </div>
+          <div class="card" id="3">
+            <div class="image-container"></div>
+            <p class="image-container__name">Промышленность</p>
+          </div>
+          <div class="card" id="4">
+            <div class="image-container"></div>
+            <p class="image-container__name">Сельское хозяйство</p>
+          </div>
+          <div class="card" id="5">
+            <div class="image-container"></div>
+            <p class="image-container__name">Наука и образование</p>
+          </div>
+          <div class="card" id="6">
+            <div class="image-container"></div>
+            <p class="image-container__name">Культура</p>
           </div>
         </div>
       </div>
